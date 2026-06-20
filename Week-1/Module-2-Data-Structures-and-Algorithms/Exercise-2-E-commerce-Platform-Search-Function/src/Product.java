8510cd5f5f992db2be1f9095fa742f2a05107092
Kunal <killerkunal45@gmail.com>
2026-06-20 19:48:32 +0000

    added product class contructor

--- Week-1/Module-2-Data-Structures-and-Algorithms/Exercise-2-E-commerce-Platform-Search-Function/src/Product.java
+++ Week-1/Module-2-Data-Structures-and-Algorithms/Exercise-2-E-commerce-Platform-Search-Function/src/Product.java
@@ -1,3 +1,16 @@
 public class Product {
     
+
+    int productId;
+    String productName;
+    String productCategory;
+
+    public Product (int productId, String productName, String productCategory){
+        
+
+        this.productId = productId;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        
+    }
 }
