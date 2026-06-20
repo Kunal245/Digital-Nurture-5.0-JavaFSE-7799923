81119afc7e07f58ea9a984b5b16a8d8823e25b93
Kunal <killerkunal45@gmail.com>
2026-06-20 20:25:12 +0000

    binary search algo

--- Week-1/Module-2-Data-Structures-and-Algorithms/Exercise-2-E-commerce-Platform-Search-Function/src/Search.java
+++ Week-1/Module-2-Data-Structures-and-Algorithms/Exercise-2-E-commerce-Platform-Search-Function/src/Search.java
@@ -4,10 +4,32 @@ public class Search {
 
         for(Product p : products){
             if(p.productName.equals(target)){
-                System.out.println("Linear Search: " + p);
+                System.out.println("Linear Search Found: " + p);
             }
         }
 
-        System.out.println("Linear Search: Product not found");
+        System.out.println("Linear Search: Product not found !!");
+    }
+
+    public static void binarySearch(Product[] products, String target){
+
+        int start = 0;
+        int end = products.length -1;
+
+        while(start <= end){
+
+            int mid = start + (end-start) / 2;
+            int cmp = products[mid].productName.compareTo(target);
+
+            if(cmp == 0){
+                System.out.println("Binary Search Found: " + products[mid]);
+            } else if(cmp < 0) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.println("Binary Search: Product not found !!");
     }
 }
