b93586e460e20aa1f215cd40f79108adfd194043
Kunal <killerkunal45@gmail.com>
2026-06-28 05:36:18 +0000

    applying 1% int to savings acc

--- Week-2/Module-3-PLSQL_Exercises/Exercise-3-Stored-Procedures/src/bank.sql
+++ Week-2/Module-3-PLSQL_Exercises/Exercise-3-Stored-Procedures/src/bank.sql
@@ -4,3 +4,23 @@ CREATE TABLE ACCOUNTS (
     AccountType VARCHAR2(20),
     Balance NUMBER
 );
+
+INSERT INTO ACCOUNTS VALUES (101,'Kunal','Savings',10000);
+INSERT INTO ACCOUNTS VALUES (102,'Rohan','Savings',15000);
+INSERT INTO ACCOUNTS VALUES (103,'Rahul','Current',20000);
+INSERT INTO ACCOUNTS VALUES (104,'Shruti','Savings',8000);
+COMMIT;
+
+SELECT * FROM ACCOUNTS;
+
+
+-- scenario-1: applying an interest rate of 1% to savings account
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
+BEGIN
+    UPDATE ACCOUNTS
+    SET Balance = Balance * 1.01
+    WHERE AccountType = 'Savings';
+    DBMS_OUTPUT.PUT_LINE('1% Interest Applied Successfully.');
+    COMMIT;
+END;
+/
