1f365649ae752b2a2fd120dc8dc33b3bfbfbd156
Kunal <killerkunal45@gmail.com>
2026-06-29 20:48:18 +0000

    adding before test after testing

---
+++ Week-1/JUnit_Basic-Testing-Exercises/Exercise-4-AAA-Pattern-JUnit/src/test/java/com/kunal/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.kunal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+
+        System.out.println("Setup Completed");
+    }
+
+    @Test
+    public void testAddition(){
+
+        int a = 10;
+        int b = 20;
+
+        int result = calculator.add(a, b);
+        assertEquals(30, result);
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+
+        System.out.println("Teardown Completed");
+    }
+}
