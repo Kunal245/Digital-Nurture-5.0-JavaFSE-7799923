fbb9bebb5033e836bda8139351d6a7748e4470a0
Kunal <killerkunal45@gmail.com>
2026-07-08 20:10:17 +0000

    added logger

--- Week-3/5-JWT-HandsOn/Create-authentication-service-that-returns-JWT/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ Week-3/5-JWT-HandsOn/Create-authentication-service-that-returns-JWT/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -1,6 +1,11 @@
 package com.cognizant.springlearn.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AuthenticationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
 
 
 }
