65bb70a6867d4e3c585afdf5f69cd82f670a3c2f
Kunal <killerkunal45@gmail.com>
2026-07-08 20:12:14 +0000

    added mapping

--- Week-3/5-JWT-HandsOn/Create-authentication-service-that-returns-JWT/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ Week-3/5-JWT-HandsOn/Create-authentication-service-that-returns-JWT/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -2,10 +2,14 @@ package com.cognizant.springlearn.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AuthenticationController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
 
+    @GetMapping("/authentication")
 
 }
